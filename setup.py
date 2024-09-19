d18e00b74dafddbe651947154544754fcc20a37f
Tristan Thrush <tristan@huggingface.co>
2024-09-19 14:57:20 -0700

    edit version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="perplexity_correlations",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=["numpy"],
     extras_require={
